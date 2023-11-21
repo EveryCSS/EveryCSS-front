755dab1c7423c336b147cb5cd3d5599dd39a36e9
Lee-Seungje <moondgod@gmail.com>
2023-11-21 23:02:58 +0900

    Add examplePage

---
+++ src/app/example.tsx
@@ -0,0 +1,9 @@
+import { Example as ExampleComponent } from "@/components";
+
+export default function Example() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <ExampleComponent />
+    </main>
+  );
+}
