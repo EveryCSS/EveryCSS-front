e710fd12aee39a90e01c617a79c05d248a48dc77
Lee-Seungje <moondgod@gmail.com>
2023-11-21 22:52:51 +0900

    Add rootState

--- src/redux/modules/index.ts
+++ src/redux/modules/index.ts
@@ -1,1 +1,14 @@
+import { combineReducers } from "redux";
+import example from "./example";
+
+const rootReducer = combineReducers({
+  example,
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+//////////////////////////////////////////////////////////////
+
 export * from "./example";
