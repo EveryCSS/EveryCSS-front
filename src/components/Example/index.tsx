f9e1e934ae51623c86e9c7b14caacac6a899e164
Lee-Seungje <moondgod@gmail.com>
2023-11-21 22:53:04 +0900

    Add example component

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+
+import { RootState } from "@/redux/modules";
+
+const Example = () => {
+  const { example1, example2, example3 } = useSelector(
+    (state: RootState) => state.example.value
+  );
+
+  return (
+    <div>
+      <h1>example1: {example1}</h1>
+      <h1>example2: {example2}</h1>
+      <h1 className={`${example3 && "bg-neutral-700"}`}>
+        example3: {example3}
+      </h1>
+    </div>
+  );
+};
+
+export default Example;
--- src/redux/modules/example.ts
+++ src/redux/modules/example.ts
@@ -1,8 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface ExampleState {
+  example1: string;
+  example2: number;
+  example3: boolean;
+}
+
+const initialState: ExampleState = {
+  example1: "",
+  example2: 0,
+  example3: true,
+};
+
 export const exampleSlice = createSlice({
   name: "example",
-  initialState: { value: { example1: "", example2: 0, example3: true } },
+  initialState: { value: initialState },
   reducers: {
     test: (state, action) => {
       state.value = action.payload;
